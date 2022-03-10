b3b812a8897cf12c2c252bceca6cf03f5fd79456
kimsh713 <kim11221122aa@gmail.com>
2022-03-10 19:56:13 +0900

    #02

--- Assets/My Project/Script/PlayerMove.cs
+++ Assets/My Project/Script/PlayerMove.cs
@@ -24,43 +24,37 @@ public class PlayerMove : MonoBehaviour
     {
         float h = 0;
         float v = 0;
-        float u = 0;
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
             h = Input.GetAxisRaw("Horizontal") * MoveSpeed * 2 * Time.deltaTime;
             v = Input.GetAxisRaw("Vertical") * MoveSpeed * 2 * Time.deltaTime;
-            u = Input.GetAxisRaw("Test") * MoveSpeed * 2 * Time.deltaTime;
         }
         else
         {
             h = Input.GetAxisRaw("Horizontal") * MoveSpeed * Time.deltaTime;
             v = Input.GetAxisRaw("Vertical") * MoveSpeed * Time.deltaTime;
-            u = Input.GetAxisRaw("Test") * MoveSpeed * Time.deltaTime;
         }
 
-        v = Mathf.Clamp(v, -0.2f, 1);
+        v = Mathf.Clamp(v, -0.07f, 1);
 
-        Vector3 movedir = new Vector3(h, u, v) + new Vector3(0, 0, 50 * Time.deltaTime);
+        Vector3 movedir = new Vector3(h, 0, v) + new Vector3(0, 0, 50 * Time.deltaTime);
         transform.Translate(movedir);
     }
 
     private void MovingWater()
     {
-        Water.transform.position = new Vector3(transform.position.x, Water.transform.position.y, transform.position.z);
+        Water.transform.position = new Vector3(Water.transform.position.x, Water.transform.position.y, transform.position.z);
     }
 
     private IEnumerator ERotation()
     {
         float x;
-        float y;
 
         while (true)
         {
             x = -Input.GetAxisRaw("Horizontal");
-            y = -Input.GetAxisRaw("Test");
-            Body.transform.rotation = Quaternion.Lerp(Body.transform.rotation, Quaternion.Euler(Vector3.forward * x * 20), Time.deltaTime * 5);
-            Body.transform.rotation = Quaternion.Lerp(Body.transform.rotation, Quaternion.Euler(Vector3.right * y * 40), Time.deltaTime * 5);
+            Body.transform.rotation = Quaternion.Lerp(Body.transform.rotation, Quaternion.Euler(Vector3.forward * x * 25), Time.deltaTime * 10);
             yield return new WaitForSeconds(0.01f);
         }
     }
